1116235a60eab3bcbeabdf39ebcfd99c8bf74721
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-08-09 22:28:52 +0200

    Card de presentación y modulo sobre mi

--- js/index.js
+++ js/index.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
 	    if (!darkMode) {
             $('#link-css').attr('href', 'css/index-dark.css');
             $('.btn-cambio-fondo img').attr('src', 'img/light-icon.png');
-            $('#icono-header').attr('src', 'img/favicon-white.png');
+            $('#icono-header').attr('src', 'img/fav-white.png');
+            $('.email img').attr('src', 'img/email-white.png');
         } else {
             $('#link-css').attr('href', 'css/index.css');
             $('.btn-cambio-fondo img').attr('src', 'img/dark-icon.png');
-            $('#icono-header').attr('src', 'img/favicon.png');
+            $('#icono-header').attr('src', 'img/fav.png');
+            $('.email img').attr('src', 'img/email.png');
         }
         darkMode = !darkMode;
 	});
